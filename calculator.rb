09b1ff5103e9c76ae0056ba25ca07f82dfa7db99
Priscilla Firpo <priscillayf@gmail.com>
2019-08-20 04:37:36 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 6
+second_number = 4
+sum = 10 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number 
